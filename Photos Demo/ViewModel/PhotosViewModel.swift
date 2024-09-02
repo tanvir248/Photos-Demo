ded08433d0282b43de613e95623254f6207355ad
Tanvir Rahman <t.rahman@nexdecade.com>
2024-09-02 13:43:44 +0600

    Setup

---
+++ Photos Demo/ViewModel/PhotosViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosViewModel.swift
+//  Photos Demo
+//
+//  Created by Tanvir Rahman on 2/9/24.
+//
+
+import Foundation
+import Alamofire
+
+class PhotosViewModel: ObservableObject {
+    @Published var viewModel : Photos? = nil
+}
